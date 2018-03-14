60a20e3d606b32de1efe9fdca9bb9d466c57ebf1
zhuwei <wei.zhu@sinobbd.com>
2018-03-14 21:59:06 +0800

    update random_choice.go

--- random_choice.go
+++ random_choice.go
@@ -1,33 +1,32 @@
-package randomchoice 
+package randomchoice
+
 import (
-    "math/rand"
-    "fmt"
-    "time"
+	"math/rand"
+	"time"
 )
 
-func RandomChoice(count int, choiceCount int) []int{
-    if  count < choiceCount{
-        choiceCount = count
-    }
+func RandomChoice(count int, choiceCount int) []int {
+	if count < choiceCount {
+		choiceCount = count
+	}
 
-    rand.Seed(time.Now().UnixNano())  
-    intSlice := make([]int, count)
-    for i:=0;i<count;i++{
-        intSlice[i] = i        
-    }
+	rand.Seed(time.Now().UnixNano())
+	intSlice := make([]int, count)
+	for i := 0; i < count; i++ {
+		intSlice[i] = i
+	}
 
-    idx := 0
-    for i:=0;i<choiceCount;i++{
-        idx = rand.Int() % count + i
-        // swap
-        swap(intSlice, i, idx)
-        count--
+	idx := 0
+	for i := 0; i < choiceCount; i++ {
+		idx = rand.Int()%count + i
+		// swap
+		swap(intSlice, i, idx)
+		count--
 
-    }
-    return intSlice[0:choiceCount]
+	}
+	return intSlice[0:choiceCount]
 }
 
-func swap(s []int, i int, j int){
-    s[i], s[j] = s[j], s[i]
+func swap(s []int, i int, j int) {
+	s[i], s[j] = s[j], s[i]
 }
-
