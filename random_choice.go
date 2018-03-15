380e253da2c2672f119e85a17dfb6b0f16dda375
zhuwei <wei.zhu@sinobbd.com>
2018-03-15 21:59:45 +0800

    update random_choice.go

--- random_choice.go
+++ random_choice.go
@@ -5,22 +5,26 @@ import (
 	"time"
 )
 
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
 func RandomChoice(count int, choiceCount int) []int {
 	if count < choiceCount {
 		choiceCount = count
 	}
 
-	rand.Seed(time.Now().UnixNano())
 	intSlice := make([]int, count)
-	for i := 0; i < count; i++ {
-		intSlice[i] = i
-	}
+    for i := 0; i < count; i++ {
+        intSlice[i] = i
+    }
 
 	idx := 0
 	for i := 0; i < choiceCount; i++ {
 		idx = rand.Int()%count + i
 		// swap
-		swap(intSlice, i, idx)
+		//swap(intSlice, i, idx)
+        intSlice[i], intSlice[idx] = intSlice[idx], intSlice[i]
 		count--
 
 	}
--- rc_test.go
+++ rc_test.go
@@ -26,28 +26,28 @@ func TestRC(t *testing.T) {
     var x []int
     x = RandomChoice(10, 3)
     if len(x) == 3{
-        t.Log("success") 
+        t.Logf("success, %v", x) 
     } else{
         t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
     }
 
     x = RandomChoice(3, 3)
     if len(x) == 3{
-        t.Log("success") 
+        t.Logf("success, %v", x) 
     } else{
         t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
     }
 
     x = RandomChoice(3, 0)
     if len(x) == 0{
-        t.Log("success") 
+        t.Logf("success, %v", x) 
     } else{
         t.Errorf("error, expect:%v, got:%v\n", 0, len(x)) 
     }
 
     x = RandomChoice(3, 4)
     if len(x) == 3{
-        t.Log("success") 
+        t.Logf("success, %v", x) 
     } else{
         t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
     }
