f55ac47a910624023a2a57703c391c3ce008d213
zhuwei <wei.zhu@sinobbd.com>
2018-03-15 21:27:30 +0800

    update rc_test.go

--- rc_test.go
+++ rc_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func Benchmark1000_3(b *testing.B) {
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		RandomChoice(1000, 3)
+	}
+}
+
 func Benchmark100_3(b *testing.B) {
 	for i := 0; i < b.N; i++ { //use b.N for looping
 		RandomChoice(100, 3)
