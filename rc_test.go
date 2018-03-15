9c04a64828634f3879f5575730d1e39fc7128371
zhuwei <wei.zhu@sinobbd.com>
2018-03-15 14:18:35 +0800

    update rc_test.go

--- rc_test.go
+++ rc_test.go
@@ -15,3 +15,36 @@ func Benchmark10_3(b *testing.B) {
 		RandomChoice(10, 3)
 	}
 }
+
+func TestRC(t *testing.T) {
+    var x []int
+    x = RandomChoice(10, 3)
+    if len(x) == 3{
+        t.Log("success") 
+    } else{
+        t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
+    }
+
+    x = RandomChoice(3, 3)
+    if len(x) == 3{
+        t.Log("success") 
+    } else{
+        t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
+    }
+
+    x = RandomChoice(3, 0)
+    if len(x) == 0{
+        t.Log("success") 
+    } else{
+        t.Errorf("error, expect:%v, got:%v\n", 0, len(x)) 
+    }
+
+    x = RandomChoice(3, 4)
+    if len(x) == 3{
+        t.Log("success") 
+    } else{
+        t.Errorf("error, expect:%v, got:%v\n", 3, len(x)) 
+    }
+}
+
+
